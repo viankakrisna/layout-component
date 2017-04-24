fb728526d35d523a06c665b9e4c57ed3d64bc95d
Ade Viankakrisna Fadlil <viankakrisna@gmail.com>
2017-04-24 13:52:24 +0700

    initial commit

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,15 +1,68 @@
-import React from 'react'
-import {render} from 'react-dom'
+import React from 'react';
+import { render } from 'react-dom';
 
-import Component from '../../src'
+import { FullHeight, Grid, Row, Col, Box, Padding } from '../../src';
 
-let Demo = React.createClass({
-  render() {
-    return <div>
-      <h1>layout-component Demo</h1>
-      <Component/>
-    </div>
-  }
-})
+class Demo extends React.Component {
+	render() {
+		return (
+			<FullHeight style={{ background: 'red' }}>
+				<Grid fluid>
+					<Row>
+						<Col md={1 / 2} style={{ background: 'purple' }}>
+							<Padding all="1em">
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis provident, in, eveniet, voluptates quis accusamus nam nihil sit iste quam error et. Voluptate, amet, velit. Provident commodi maiores, quis quae?
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis provident, in, eveniet, voluptates quis accusamus nam nihil sit iste quam error et. Voluptate, amet, velit. Provident commodi maiores, quis quae?
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis provident, in, eveniet, voluptates quis accusamus nam nihil sit iste quam error et. Voluptate, amet, velit. Provident commodi maiores, quis quae?
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis provident, in, eveniet, voluptates quis accusamus nam nihil sit iste quam error et. Voluptate, amet, velit. Provident commodi maiores, quis quae?
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis provident, in, eveniet, voluptates quis accusamus nam nihil sit iste quam error et. Voluptate, amet, velit. Provident commodi maiores, quis quae?
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis provident, in, eveniet, voluptates quis accusamus nam nihil sit iste quam error et. Voluptate, amet, velit. Provident commodi maiores, quis quae?
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis provident, in, eveniet, voluptates quis accusamus nam nihil sit iste quam error et. Voluptate, amet, velit. Provident commodi maiores, quis quae?
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis provident, in, eveniet, voluptates quis accusamus nam nihil sit iste quam error et. Voluptate, amet, velit. Provident commodi maiores, quis quae?
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis provident, in, eveniet, voluptates quis accusamus nam nihil sit iste quam error et. Voluptate, amet, velit. Provident commodi maiores, quis quae?
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis provident, in, eveniet, voluptates quis accusamus nam nihil sit iste quam error et. Voluptate, amet, velit. Provident commodi maiores, quis quae?
+							</Padding>
+						</Col>
+						<Col md={1 / 2} style={{ background: 'yellow' }}>
+							<Box ar={1 / 1}>
+								Test
+							</Box>
+						</Col>
+						<Col md={1 / 3} style={{ background: 'blue' }}>
+							<Box ar={9 / 16}>
+								<Row>
+									<Col md={1 / 2} style={{ background: 'red' }}>
+										<Padding all="1em">
+											Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis provident, in, eveniet, voluptates quis accusamus nam nihil sit iste quam error et. Voluptate, amet, velit. Provident commodi maiores, quis quae?
+											Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis provident, in, eveniet, voluptates quis accusamus nam nihil sit iste quam error et. Voluptate, amet, velit. Provident commodi maiores, quis quae?
+											Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis provident, in, eveniet, voluptates quis accusamus nam nihil sit iste quam error et. Voluptate, amet, velit. Provident commodi maiores, quis quae?
+											Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis provident, in, eveniet, voluptates quis accusamus nam nihil sit iste quam error et. Voluptate, amet, velit. Provident commodi maiores, quis quae?
+											Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis provident, in, eveniet, voluptates quis accusamus nam nihil sit iste quam error et. Voluptate, amet, velit. Provident commodi maiores, quis quae?
+											Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis provident, in, eveniet, voluptates quis accusamus nam nihil sit iste quam error et. Voluptate, amet, velit. Provident commodi maiores, quis quae?
+											Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis provident, in, eveniet, voluptates quis accusamus nam nihil sit iste quam error et. Voluptate, amet, velit. Provident commodi maiores, quis quae?
+											Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis provident, in, eveniet, voluptates quis accusamus nam nihil sit iste quam error et. Voluptate, amet, velit. Provident commodi maiores, quis quae?
+											Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis provident, in, eveniet, voluptates quis accusamus nam nihil sit iste quam error et. Voluptate, amet, velit. Provident commodi maiores, quis quae?
+											Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis provident, in, eveniet, voluptates quis accusamus nam nihil sit iste quam error et. Voluptate, amet, velit. Provident commodi maiores, quis quae?
+										</Padding>
+									</Col>
+									<Col md={1 / 2} style={{ background: 'orange' }}>
+										<Box ar={1 / 1}>
+											Test
+										</Box>
+									</Col>
+									<Col md={1 / 3} style={{ background: 'indigo' }}>
+										<Box ar={9 / 16}>
+											Test
+										</Box>
+									</Col>
+								</Row>
+							</Box>
+						</Col>
+					</Row>
+				</Grid>
+			</FullHeight>
+		);
+	}
+}
 
-render(<Demo/>, document.querySelector('#demo'))
+render(<Demo />, document.querySelector('#demo'));
--- src/index.js
+++ src/index.js
@@ -1,9 +1,94 @@
-import React from 'react'
+import React from 'react';
+import styled, { injectGlobal } from 'styled-components';
 
-export default React.createClass({
-  render() {
-    return <div>
-      <h2>Welcome to React components</h2>
-    </div>
+injectGlobal`
+	body {
+		margin: 0;
+		padding: 0;
+		font-family: sans-serif;
+	}
+`;
+
+export class FullHeight extends React.Component {
+	state = {
+		height: window.innerHeight,
+	};
+	calculateHeight = e => window.innerHeight;
+
+	componentDidMount() {
+		this.calculateHeight();
+		window.addEventListener('resize', this.calculateHeight);
+	}
+	render() {
+		return (
+			<div style={{ minHeight: this.state.height, ...this.props.style }}>
+				{this.props.children}
+			</div>
+		);
+	}
+}
+
+export const Box = styled(props => (
+	<div className={props.className}>
+		<div>
+			{props.children}
+		</div>
+	</div>
+))`
+	&:before {
+		content: '';
+		display: block;
+		padding-top: ${props => props.ar * 100}%;
+	}
+	position: relative;
+	> div {
+		position: absolute;
+		top: 0;
+		left: 0;
+		right: 0;
+		bottom: 0;
+	}
+`;
+
+export const Column = styled.div`
+  flex: ${props => props.flex || 1} ${props => props.xs * 100 || 100}%;
+  width: ${props => props.xs * 100 || 100}%;
+
+  @media (min-width: 768px){
+    flex: ${props => props.flex || 1} ${props => (props.sm || props.xs) * 100 || 100}%;
+    width: ${props => (props.sm || props.xs) * 100 || 100}%;
   }
-})
+
+  @media (min-width: 992px){
+    flex: ${props => props.flex || 1} ${props => (props.md || props.sm) * 100 || 100}%;
+    width: ${props => (props.md || props.sm) * 100 || 100}%;
+  }
+
+  @media (min-width: 1200px){
+    flex: ${props => props.flex || 1} ${props => (props.lg || props.md) * 100 || 100}%;
+    width: ${props => (props.lg || props.md) * 100 || 100}%;
+  }
+
+`;
+
+export const Col = Column;
+
+export const Row = styled.div`
+  display: flex;
+  flex-flow: row wrap;
+`;
+
+export const Grid = styled.div`
+  width: 100%;
+  max-width: ${props => (props.fluid ? '100%' : '1160px')};
+  margin: auto;
+  box-sizing: border-box;
+`;
+
+export const Padding = styled.div`
+	padding: ${props => props.padding};
+	padding: ${props => String(props.all) + (Number.isInteger(props.all) ? 'px' : '')};
+	padding: ${props => [props.top, props.right, props.bottom, props.left].map(e => String(e) + (Number.isInteger(e) ? 'px' : ''))}
+`;
+
+export default Grid;
